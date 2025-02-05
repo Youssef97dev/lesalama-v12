8924c1bbf669fe421054903b330b9c34fba18b59
Youssef97dev <youssefmoussafi97@gmail.com>
2025-02-05 14:24:06 +0100

    change images dimension

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,10 +10,10 @@ const images = [
 ];
 
 const imagesMobile = [
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/Salama/salama-sah-1_bavl73",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/Salama/salama-sah-3_l2zn1q",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/salamaV10/bgc6cpn0bkmf1rsz2dpd",
-  "https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/salamaV10/ukjkpsvpq0cxkhy7olzt",
+  "https://res.cloudinary.com/dz7wroord/image/upload/c_thumb,h_640,w_480/Salama/salama-sah-1_bavl73",
+  "https://res.cloudinary.com/dz7wroord/image/upload/c_thumb,h_640,w_480/Salama/salama-sah-3_l2zn1q",
+  "https://res.cloudinary.com/dz7wroord/image/upload/c_thumb,h_640,w_480/salamaV10/bgc6cpn0bkmf1rsz2dpd",
+  "https://res.cloudinary.com/dz7wroord/image/upload/c_thumb,h_640,w_480/salamaV10/ukjkpsvpq0cxkhy7olzt",
 ];
 
 const Hero = () => {
