285c84015a07a727aa14d81bed88949e386f5394
Youssef97dev <youssefmoussafi97@gmail.com>
2025-02-05 15:56:01 +0100

    navbar update 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,14 +16,14 @@ const Navbar = () => {
   return (
     <>
       <nav className="fixed left-0 top-0 z-20 w-[25%] h-full lg:flex flex-col justify-between items-start gap-7 px-3 bg-gradient-to-r from-primary_2 to-transparent hidden">
-        <div className="w-fit flex justify-start items-start">
+        <Link href="/" className="w-fit flex justify-start items-start">
           <Image
             src="/logo-salama-humberger.png"
             width={180}
             height={180}
             className="cursor-pointer w-52 h-52 z-30"
           />
-        </div>
+        </Link>
         <ul className="w-1/2 flex flex-col justify-start items-start gap-4 text-[18px] leading-[24px] uppercase font-light px-6">
           <li className="w-full px-3 py-2 hover:bg-primary hover:text-primary_1 transition-colors duration-150 ease-in-out">
             <Link href="/">Home</Link>
@@ -74,14 +74,14 @@ const Navbar = () => {
       </nav>
 
       <nav className="fixed z-30 left-0 top-0 w-full flex justify-between items-center py-1 px-5 bg-primary_2 lg:hidden">
-        <div className="w-fit flex justify-center items-center">
+        <Link href="/" className="w-fit flex justify-center items-center">
           <Image
             src="/logo-salama-humberger.png"
             width={150}
             height={150}
             className="cursor-pointer w-20 h-20"
           />
-        </div>
+        </Link>
         <div
           className="text-black transition-transform duration-500 pl-2"
           onClick={() => setIsOpen(!isOpen)}
