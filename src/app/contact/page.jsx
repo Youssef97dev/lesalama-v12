eff52564d93d3c16fc80cbfc388f897814ff65f1
Youssef97dev <youssefmoussafi97@gmail.com>
2025-02-07 11:16:00 +0100

    add contact form 2

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,7 +5,7 @@ import ContactForm from "./components/ContactForm";
 
 const page = () => {
   return (
-    <div className="w-full flex flex-col justify-center items-center gap-2 pb-28 px-2 lg:px-80">
+    <div className="w-full flex flex-col justify-center items-center gap-6 pb-28 px-2 lg:px-80">
       <div className="w-full flex flex-col lg:flex-row-reverse justify-start gap-5 lg:justify-between items-center text-primary">
         <Mapbox />
         <Details />
