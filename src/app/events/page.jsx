1959ce1391589b64705b8b9a2e4d122cd851beca
Youssef97dev <youssefmoussafi97@gmail.com>
2025-02-08 11:00:14 +0100

    add access code to events

---
+++ src/app/events/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="w-full text-primary text-center">page events</div>;
+};
+
+export default page;
--- src/app/guests/components/Guests.jsx
+++ src/app/guests/components/Guests.jsx
@@ -1,9 +1,28 @@
 "use client";
+import { useState } from "react";
 import Image from "next/image";
 import { useTranslation } from "react-i18next";
+import { useRouter } from "next/navigation";
+import { LuShieldAlert } from "react-icons/lu";
+import guests from "../../../data/guestAccess.json";
 
 const Guests = () => {
+  const [accessCode, setAccessCode] = useState("");
+  const [errorCode, setErrorCode] = useState(false);
+
+  const router = useRouter();
+
   const { t } = useTranslation();
+
+  const validateGuests = async (code) => {
+    const codeExist = await guests.filter((guest) => guest.guestCode === code);
+    if (codeExist.length > 0) {
+      router.push(`/events?accessCode=${code}`);
+    } else {
+      setErrorCode(true);
+    }
+  };
+
   return (
     <div className="relative w-full h-screen">
       <Image
@@ -22,14 +41,27 @@ const Guests = () => {
           />
           <div className="w-full flex justify-center items-center">
             <input
+              value={accessCode}
+              onChange={(e) => setAccessCode(e.target.value)}
               type="text"
               placeholder={t("events.placeholder")}
               className="p-3 text-black focus:outline-none"
             />
-            <button className="py-3 px-4 bg-[#CA8A04]">
+            <button
+              className="py-3 px-4 bg-[#CA8A04]"
+              onClick={() => validateGuests(accessCode)}
+            >
               {t("events.button")}
             </button>
           </div>
+          {errorCode && (
+            <div className="flex justify-center items-center gap-3 px-16 py-2 text-[#DC2626] bg-[#FECACA]">
+              <LuShieldAlert size={18} />
+              <span className="text-[12.8px]  leading-[20px]  font-medium">
+                Incorrect access code
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
