e5b4d62920212117bf788de9a358ca3a8b77189b
Youssef97dev <youssefmoussafi97@gmail.com>
2025-02-11 13:28:15 +0100

    valid guests suspen test

--- src/app/menu-group/page.jsx
+++ src/app/menu-group/page.jsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 
 const page = () => {
   return (
-    <Suspense>
+    <Suspense fallback={<div>test</div>}>
       <div className="w-full text-primary text-center mb-20 mt-28 lg:mt-5">
         <Slides />
       </div>
