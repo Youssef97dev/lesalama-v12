41d01311f0c0f2c535de0df0a793dec9f274fe6a
Youssef97dev <youssefmoussafi97@gmail.com>
2025-02-11 12:03:02 +0100

    add menu group 4

--- src/app/menu-group/components/Slides.jsx
+++ src/app/menu-group/components/Slides.jsx
@@ -9,7 +9,9 @@ import "swiper/css/effect-cube";
 import "swiper/css/pagination";
 
 // import required modules
-import { EffectCube, Pagination } from "swiper/modules";
+import { EffectCube, Pagination, Navigation } from "swiper/modules";
+
+import { RiArrowRightSLine, RiArrowLeftSLine } from "react-icons/ri";
 
 const Slides = () => {
   return (
@@ -24,7 +26,11 @@ const Slides = () => {
           shadowScale: 0.94,
         }}
         pagination={true}
-        modules={[EffectCube, Pagination]}
+        modules={[EffectCube, Pagination, Navigation]}
+        navigation={{
+          nextEl: ".swiper-button-next-ex1",
+          prevEl: ".swiper-button-prev-ex1",
+        }}
         className="mySwiper"
       >
         <SwiperSlide>
@@ -42,6 +48,12 @@ const Slides = () => {
         <SwiperSlide>
           <img src="https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/menu%20group%20salama%20v2/i1kbrsui0kmbjq3eehzq" />
         </SwiperSlide>
+        <button className="swiper-button-prev-ex1 grid place-content-center left-2 lg:-left-16 px-0.5 py-0.5 lg:px-4 lg:py-4 transition text-primary hover:text-white border border-primary  hover:border-primary hover:bg-primary rounded-full absolute z-[999] top-1/2 -translate-y-1/2">
+          <RiArrowLeftSLine size={25} />
+        </button>
+        <button className="swiper-button-next-ex1 grid place-content-center right-2 lg:-right-16 px-0.5 py-0.5 lg:px-4 lg:py-4 transition text-primary hover:text-white border border-primary  hover:border-primary hover:bg-primary rounded-full absolute z-[999] top-1/2 -translate-y-1/2">
+          <RiArrowRightSLine size={25} />
+        </button>
       </Swiper>
     </>
   );
